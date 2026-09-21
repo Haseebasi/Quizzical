88a0a2f9b5d8814e9ac8189f2a8145bb6099a55b
Haseebasi <haseebasi005@gmail.com>
2026-09-21 11:08:44 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import { useState } from 'react'
+import he from 'he'
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [start,setStart]= React.useState(false)
+  function startQuiz(){
+    setStart(true)
+  }
   return (
     <>
       <div className="top-right-shape"></div>
       <div className="bottom-left-shape"></div>
 
-      <section className="hero">
+      {!start && (<section className="hero">
         <h1>Quizzical</h1>
         <p>Are you pro on anime ?</p>
-        <div className="start-button">Start quiz</div>
-      </section>
+        <div className="start-button" onClick={startQuiz}>Start quiz</div>
+      </section>)}
     </>
   )
 }
