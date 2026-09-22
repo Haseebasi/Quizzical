b6254e93c0e8b2cdcff9f50fa838b26538280587
Haseebasi <haseebasi005@gmail.com>
2026-09-22 16:39:02 +0530

    updated functionality of questions app component

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,25 +1,47 @@
 import { useState,useEffect } from "react"
 import he from "he"
+import {clsx} from "clsx"
+
+
+const keysOfselected=Object.keys(props.selectedValues)
+const valuesOfselected = Object.values(props.selectedValues)
 export default function Question(props){
     const [options,setOptions] = useState([])
     useEffect(() => {
     const shuffled = pushRandom(props.data.incorrect_answers, props.data.correct_answer);
     setOptions(shuffled);
   }, [props.data.incorrect_answers, props.data.correct_answer]);
-    const optionButtons = options.map((option, index) => (
+    const optionButtons = options.map((option, index) => {
+        const isAnswered = props.isSubmitted && keysOfselected.includes(String(props.id))
+        const indexInSelected = isAnswered && keysOfselected.indexOf(String(props.id))
+        const isSelected=isAnswered && valuesOfselected[indexInSelected] === option
+        // const isScored=isSelected && props.correctAnswers[props.id] === option
+        const isCorrect = isAnswered && props.data.correct_answer
+        const isWrong=isSelected && !props.correctAnswers[props.id] === option
+        
     
-    <label 
+    return(<label 
         key={`q${props.id}opt${index}`} 
         htmlFor={`q${props.id}opt${index}`}>
     <input 
-        type="radio" id={`q${props.id}opt${index}`} 
-        name={props.id} value={option} disabled={props.isSubmitted}/>
-    <span className="btn-pill">
+        type="radio" 
+        id={`q${props.id}opt${index}`} 
+        name={props.id} value={option} 
+        disabled={props.isSubmitted}
+        />
+    <span className={clsx("option-pill",
+    {
+        selected:isSelected,
+        "wrong-answer":isWrong,
+        "correct-answer":isCorrect
+    }
+    )}>
             {he.decode(option)}
     </span>
-  </label>
-    
-))
+  </label>)
+}
+  
+)
     return(
         <section className="question-pad">
             <p>{he.decode(props.data.question)}</p>
