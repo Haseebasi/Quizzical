b165438babd05be9334ebf438d14ac3970065551
Haseebasi <haseebasi005@gmail.com>
2026-09-23 13:50:45 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Quizzical/',
+  base: '/Quizzical/', // Ensures all asset paths prepend /Quizzical/ in build
 })
